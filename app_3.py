11c9afe0bc58f45f18f6ad7e8561d2ec6fbc4faf
PC <aluisaybar@gmail.com>
2026-05-07 20:57:33 -0500

    test 2

--- app_3.py
+++ app_3.py
@@ -14,7 +14,10 @@ import pytz
 import os
 
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:FVoHPDLfCIfJYToNBBCiOypwjPgLQXtf@turntable.proxy.rlwy.net:26321/railway"
+#
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:FVoHPDLfCIfJYToNBBCiOypwjPgLQXtf@turntable.proxy.rlwy.net:26321/railway"
+
+SQLALCHEMY_DATABASE_URL = os.environ["SQLALCHEMY_DATABASE_URL"]
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 metadata = MetaData()
